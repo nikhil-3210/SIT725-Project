77c2edd50171309ee02fce1f74503ef195552d6d
Nikhil Jadav <nikhil.jadhav3210@gmail.com>
2025-02-06 09:07:40 +1100

    design added and docker file config added

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,20 +1,20 @@
 const loadNavbar = () => {
   const token = localStorage.getItem("token");
-  const role = localStorage.getItem("role"); // Get the user's role
+  const role = localStorage.getItem("role");
 
-  // Dynamically create navbar
   const navbarHTML = `
     <nav class="navbar">
+      <a href="posts.html" class="brand-logo">Logo</a>
       <ul>
         <li><a href="posts.html">Home</a></li>
         ${
           token && role === "donor"
             ? '<li><a href="createPost.html">Create Post</a></li>'
             : ""
-        } <!-- Show Create Post for donors -->
+        }
         ${token ? '<li><a href="profile.html">Profile</a></li>' : ""}
-        <li><a href="blogs.html">Blogs</a></li> <!-- New link for Blogs -->
-        <li><a href="contact.html">Contact Us</a></li> <!-- New link for Contact Us -->
+        <li><a href="blogs.html">Blogs</a></li>
+        <li><a href="contact.html">Contact Us</a></li>
         ${
           token
             ? '<li><button id="logoutButton" class="logout-btn">Logout</button></li>'
@@ -24,20 +24,15 @@ const loadNavbar = () => {
     </nav>
   `;
 
-  // Insert the navbar at the top of the body
-  const body = document.body;
-  body.insertAdjacentHTML("afterbegin", navbarHTML);
+  document.body.insertAdjacentHTML("afterbegin", navbarHTML);
 
-  // Add logout functionality
   if (token) {
-    const logout = () => {
+    document.getElementById("logoutButton")?.addEventListener("click", () => {
       localStorage.removeItem("token");
       localStorage.removeItem("role");
-      window.location.href = "index.html"; // Redirect to login page
-    };
-    document.getElementById("logoutButton")?.addEventListener("click", logout);
+      window.location.href = "index.html";
+    });
   }
 };
 
-// Load the navbar when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", loadNavbar);
--- src/server.js
+++ src/server.js
@@ -27,5 +27,5 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2999;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
